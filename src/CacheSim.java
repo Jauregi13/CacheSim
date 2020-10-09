5c700070258cc06444725fa2770505731c11688c
jauregi13 <jauregi13@jon.j1305@gmail.com>
2020-10-09 18:12:51 +0200

    cache taula sortuta

--- src/CacheSim.java
+++ src/CacheSim.java
@@ -2,21 +2,27 @@ import java.util.Scanner;
 
 public class CacheSim {
 
+	private static final int BLOKEAK = 8;
+	private static final int TCM = 2;
+	private static final int TMN = 20;
+	
 	private static int hitza;
 	private static int blokea;
 	private static int tag;
 	private static int multzo;
 	private static int helbide;
-	//private static int[][] cacheTaula;
+	private static Integer[][] cacheTaula;
 	private static boolean hutsegitea = true;
-	private static final int BLOKEAK = 8;
+	private static int multzo_kop;
+	private static int bloke_kop_multzoko;
+	
 	private static int ordezk_polit;
 	private static int irakurri;
+	
 	public static void main(String[] args) {
 		
 		int hitz_tam;
 		int blok_tam;
-		int bloke_kop_multzoko;
 		
 		Scanner scan = new Scanner(System.in);
 		
@@ -32,10 +38,10 @@ public class CacheSim {
 		System.out.print("Helbidea (byte) (-1 uzteko) >");
 		helbide = scan.nextInt();
 		
-		int multzo_kop = BLOKEAK / bloke_kop_multzoko;
+		multzo_kop = BLOKEAK / bloke_kop_multzoko;
 		
 		
-		Integer[][] cacheTaula = new Integer[8][5];
+		cacheTaula = new Integer[8][5];
 		
 		while (helbide != -1) {
 			
@@ -57,16 +63,27 @@ public class CacheSim {
 			System.out.println("Helbidea: " + helbide + " - Hitza: " + hitza + " - Blokea: " + blokea +
 					" (" + hitza + " - " + (hitza+BLOKEAK-1) + " hitzak)");
 			
-			multzoKalkulua(multzo_kop);
+			multzoKalkulua();
 			
-			helbideaCacheTaulanSartu(multzo_kop,cacheTaula);
+			helbideaCacheTaulanSartu();
+			
+			int t_totala = 0;
 			
 			if(hutsegitea) {
 				System.out.println("HUTSEGITEA");
+				System.out.println("Atzipen-denbora: cachean bilatzea, "+ TCM + " -- bloke bat eramatea"
+						+ "(M>C,C>M), " +(TMN +hitz_blokeko));
+				t_totala = TCM+TMN+hitz_blokeko;
 			}
 			else {
 				System.out.println("ASMATZEA");
+				System.out.println("Atzipen-denbora: cachean bilatzea, "+ TCM);
+				t_totala = TCM;
 			}
+			
+			System.out.println("T_atz: " + t_totala + " ziklo");
+			
+			cacheTaulaSortu();
 			
 			System.out.print("Helbidea (byte) (-1 uzteko) >");
 			helbide = scan.nextInt();
@@ -76,7 +93,45 @@ public class CacheSim {
 		
 	}
 	
-	public static void multzoKalkulua(int multzo_kop) {
+	private static void cacheTaulaSortu() {
+		
+		
+		System.out.println("  okup ald  tag  ord ||  blokea");
+		System.out.println("----------------------------------");
+		
+		for (int i = 0; i < multzo_kop; i++) {
+			
+			for (int j = 0; j < bloke_kop_multzoko; j++) {
+				
+				for (int j2 = 0; j2 < cacheTaula[j].length; j2++) {
+					
+					if(j2 == 4) {
+						
+						if(cacheTaula[i*bloke_kop_multzoko+j][j2] == null)
+							System.out.print(" ||   ---  ");
+						else {
+							System.out.print(" ||   b" + cacheTaula[i*bloke_kop_multzoko+j][j2] + "  ");
+						}
+					}
+					else if(cacheTaula[i*bloke_kop_multzoko+j][j2] == null) {
+						System.out.print("   0 ");
+					}
+					else {
+						System.out.print("   " + cacheTaula[i*bloke_kop_multzoko+j][j2] + " ");
+					}
+				}
+				
+				System.out.println("");
+			}
+			
+			System.out.println("----------------------------------");
+			
+			
+		}
+		
+	}
+
+	private static void multzoKalkulua() {
 		
 		if(multzo_kop == 1) {
 			
@@ -101,30 +156,30 @@ public class CacheSim {
 	}
 	
 	
-	public static void helbideaCacheTaulanSartu(int multzo_kop, Integer[][] cacheTaula) {
+	private static void helbideaCacheTaulanSartu() {
 		
 		if(multzo_kop != 8) { 
 			int i = 0;
 			boolean aurkitua = false;
-			while(i < cacheTaula.length / multzo_kop && !aurkitua) {
+			while(i < bloke_kop_multzoko && !aurkitua) {
 				
-				if(cacheTaula[(cacheTaula.length / multzo_kop)*multzo +i][4] == null) {
+				if(cacheTaula[bloke_kop_multzoko*multzo +i][4] == null) {
 					
-					cacheTaula[(cacheTaula.length / multzo_kop)*multzo +i][0] = 1;
-					cacheTaula[(cacheTaula.length / multzo_kop)*multzo+i][1] = 1;
-					cacheTaula[(cacheTaula.length / multzo_kop)*multzo+i][2] = tag;
-					cacheTaula[(cacheTaula.length / multzo_kop)*multzo+i][3] = 3;
-					cacheTaula[(cacheTaula.length / multzo_kop)*multzo+i][4] = blokea;
+					cacheTaula[bloke_kop_multzoko*multzo +i][0] = 1;
+					cacheTaula[bloke_kop_multzoko*multzo+i][1] = 1;
+					cacheTaula[bloke_kop_multzoko*multzo+i][2] = tag;
+					cacheTaula[bloke_kop_multzoko*multzo+i][3] = 3;
+					cacheTaula[bloke_kop_multzoko*multzo+i][4] = blokea;
 					
 					hutsegitea = true;
 					aurkitua = true;
 				}
-				else if(cacheTaula[(cacheTaula.length / multzo_kop)*multzo +i][4] == blokea) {
+				else if(cacheTaula[bloke_kop_multzoko*multzo +i][4] == blokea) {
 					
 					hutsegitea = false;
 					aurkitua = true;
 				}
-				else if(cacheTaula[(cacheTaula.length / multzo_kop)*multzo +i][4] != null && cacheTaula[(cacheTaula.length / multzo_kop)*multzo +i][4] != blokea) {
+				else if(cacheTaula[bloke_kop_multzoko*multzo +i][4] != null && cacheTaula[bloke_kop_multzoko*multzo +i][4] != blokea) {
 					
 					aurkitua = false;
 				}
